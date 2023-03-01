622c60dd2e8e6518090454314366d085d4191e7c
Florian Heubeck <heubeck@mediamarktsaturn.com>
2023-03-01 17:38:55 +0100

    chore(test): Local repo analysis test

---
+++ src/test/java/com/mediamarktsaturn/ghbot/sbom/LocalRepositoryAnalysis.java
@@ -0,0 +1,50 @@
+package com.mediamarktsaturn.ghbot.sbom;
+
+import java.io.File;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.mediamarktsaturn.ghbot.git.TechnolinatorConfig;
+import io.quarkus.logging.Log;
+import io.quarkus.test.junit.QuarkusTest;
+
+// to be used for manual use only
+@Disabled
+@QuarkusTest
+public class LocalRepositoryAnalysis {
+
+    @Inject
+    CdxgenClient cdxgenClient;
+
+    final ObjectMapper configMapper = new ObjectMapper(new YAMLFactory());
+
+    String dir = "/home/heubeck/w/tmp/technolinator";
+
+    @Language("yml")
+    String configString = """
+        analysis:
+            recursive: false
+        """;
+
+    @Test
+    public void runLocalAnalysis() throws Exception {
+        var folder = new File(dir);
+        if (!folder.exists() || !folder.canRead()) {
+            throw new IllegalArgumentException("Cannot access " + dir);
+        }
+        var projectName = folder.getName();
+
+        TechnolinatorConfig config = configMapper.readValue(configString, TechnolinatorConfig.class);
+
+        var result = cdxgenClient.generateSBOM(folder, projectName, Optional.of(config))
+            .await().indefinitely();
+        Log.infof("Analysis success, got: %s", result.getClass().getSimpleName());
+    }
+}
