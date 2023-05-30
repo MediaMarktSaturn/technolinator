d8ecf2fe2f5311f38d7ef64960dcebcad995f8fb
Florian Heubeck <heubeck@mediamarktsaturn.com>
2023-05-30 09:10:08 +0200

    chore(test): Add gradle single-module test project

--- src/test/java/com/mediamarktsaturn/technolinator/sbom/CdxgenClientGenerationTest.java
+++ src/test/java/com/mediamarktsaturn/technolinator/sbom/CdxgenClientGenerationTest.java
@@ -10,6 +10,8 @@ import java.util.Optional;
 
 import org.cyclonedx.model.Component;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.mediamarktsaturn.technolinator.Command;
 import com.mediamarktsaturn.technolinator.ConfigBuilder;
@@ -175,6 +177,29 @@ class CdxgenClientGenerationTest {
         assertThat(result).isInstanceOfSatisfying(Result.Success.class, s -> {
             assertThat(s.result()).isInstanceOfSatisfying(CdxgenClient.SBOMGenerationResult.Fallback.class, fallback -> {
                 assertThat(fallback.sbom().getComponents()).flatExtracting(Component::getName).contains("ktor-client-serialization", "mimic-fn");
+            });
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "gradle-single-module, false",
+        "gradle-single-module-kt, false",
+        "gradle-single-module, true",
+        "gradle-single-module-kt, true"
+    })
+    void testSimpleGradleProject(String repoName, boolean recursive) {
+        // Given
+        var file = Paths.get("src/test/resources/repo/" + repoName);
+        var config = ConfigBuilder.create().analysis(new TechnolinatorConfig.AnalysisConfig(null, recursive, List.of())).enable(true).build();
+
+        // When
+        var result = generateSBOM(file, repoName, Optional.of(config));
+
+        // Then
+        assertThat(result).isInstanceOfSatisfying(Result.Success.class, s -> {
+            assertThat(s.result()).isInstanceOfSatisfying(CdxgenClient.SBOMGenerationResult.Proper.class, proper -> {
+                assertThat(proper.sbom().getComponents()).flatExtracting(Component::getName).contains("quarkus-core", "maven-model");
             });
         });
     }
