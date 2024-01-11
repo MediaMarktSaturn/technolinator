1936dda7bd82904cb985f722350bfee666108680
Tung Beier <44575474+beiertu-mms@users.noreply.github.com>
2024-01-11 12:44:28 +0100

    fix: adjust parsing of app.allowed_env_substitutions (#304)
    SmallRyeConfig `getValue` doesn't allow the provided configured value to
    be empty, therefore use `getOptionalValue` instead.
    
    See also: https://github.com/eclipse/microprofile-config/pull/407
    
    Fixes #303

---
+++ src/test/java/com/mediamarktsaturn/technolinator/CustomTestProfiles.java
@@ -0,0 +1,23 @@
+package com.mediamarktsaturn.technolinator;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class CustomTestProfiles {
+
+    public static class AllowedEnvSubstitutions implements QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Collections.singletonMap("app.allowed_env_substitutions", "substitution.1,substitution.2");
+        }
+    }
+
+    public static class EmptyAllowedEnvSubstitutions implements QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Collections.singletonMap("app.allowed_env_substitutions", "   ");
+        }
+    }
+}
