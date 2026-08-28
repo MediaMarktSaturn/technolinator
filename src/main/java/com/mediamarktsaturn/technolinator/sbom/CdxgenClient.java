243d54ffa98ad8c4dc068c9f0c52f2d8d3047060
Florian Heubeck <heubeck@mediamarktsaturn.com>
2026-08-28 15:46:33 +0200

    feat: Update to cyclonedx version 1.7

--- src/main/java/com/mediamarktsaturn/technolinator/sbom/CdxgenClient.java
+++ src/main/java/com/mediamarktsaturn/technolinator/sbom/CdxgenClient.java
@@ -151,7 +151,7 @@ public class CdxgenClient {
      * * --project-name %s # name of main component of the SBOM, defaulting to the repository name
      * * --no-validate # disable cdxgen validation as we try to process everything
      */
-    private static final String CDXGEN_CMD_FMT = "cdxgen --spec-version 1.5 -o %s%s%s%s%s%s --project-name %s --no-validate";
+    private static final String CDXGEN_CMD_FMT = "cdxgen --spec-version 1.7 -o %s%s%s%s%s%s --project-name %s --no-validate";
 
     public record SbomCreationCommand(
         Path repoDir,
